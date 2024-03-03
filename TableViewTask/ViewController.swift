42a7009d646fbb4f1287c4580370a0b9d9b82981
Saleh <saleh.binessa@gmail.com>
2024-03-03 13:26:38 +0300

    Initial Commit

---
+++ TableViewTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewTask
+//
+//  Created by Saleh Bin Essa on 03/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
